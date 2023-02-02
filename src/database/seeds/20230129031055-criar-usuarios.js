698510b259d17a1983b8a4daa7b014924e0a2bc4
Herco ZauZau <zauzauherco@gmail.com>
2023-02-01 16:06:24 -1200

    adicionados seed e index da DB

---
+++ src/database/seeds/20230129031055-criar-usuarios.js
@@ -0,0 +1,37 @@
+const bcryptjs = require('bcryptjs');
+
+/**
+ * Usamos seeds para criar valores iniciais no banco de dados, fora os valores que serão criados pelos usuários.
+*/
+
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert(
+    'users',
+    [
+      {
+        nome: 'Ana Sara',
+        email: 'anasara@email.com',
+        password_hash: await bcryptjs.hash('12345', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Marcos Cossa',
+        email: 'mcossa@email.com',
+        password_hash: await bcryptjs.hash('12345', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nome: 'Milton Chissano',
+        email: 'milton@email.com',
+        password_hash: await bcryptjs.hash('12345', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+    {},
+  ),
+
+  down: () => {},
+};
