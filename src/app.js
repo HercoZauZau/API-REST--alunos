2e9b096026a102e5d0310469adee2e2fb78d25fa
Herco ZauZau <zauzauherco@gmail.com>
2023-05-28 19:29:16 -1200

    atualizado

--- src/app.js
+++ src/app.js
@@ -1,43 +1,20 @@
-import dotenv from "dotenv";
-import { resolve } from "path";
+import dotenv from 'dotenv';
+import { resolve } from 'path';
 
 dotenv.config();
 
-import "./database";
+import './database';
 
-import express from "express";
-import helmet from "helmet";
-import homeRoutes from "./routes/homeRoutes";
-import userRoutes from "./routes/userRoutes";
-import tokenRoutes from "./routes/tokenRoutes";
-import alunoRoutes from "./routes/alunoRoutes";
-import fotoRoutes from "./routes/fotoRoutes";
+// import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import tokenRoutes from './routes/tokenRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+import fotoRoutes from './routes/fotoRoutes';
 
-class App {
-  constructor() {
-    this.app = express();
-    this.middlewares();
-    this.routes();
-  }
-
-  middlewares() {
-    this.app.use(helmet());
-    this.app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, '..', 'uploads')));
-  }
-
-  routes() {
-    this.app.use("/", homeRoutes);
-    this.app.use("/users/", userRoutes);
-    this.app.use("/tokens/", tokenRoutes);
-    this.app.use("/alunos/", alunoRoutes);
-    this.app.use("/fotos/", fotoRoutes);
-  }
-}
-
-/* 
+/*
 
   Use o seguinte trecho caso precise:
     * Limitar os endereços que possam ter acesso a sua API;
@@ -47,26 +24,43 @@ class App {
 
 */
 
-/*
+// const whiteList = [
+//   'http://localhost:3001', // exemplo
+// ];
 
-import cors from 'cors';
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (whiteList.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
 
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
 
-const whiteList = [
-  'http://localhost:3001', // exemplo
-];
+  middlewares() {
+    // this.app.use(cors(corsOptions));
+    this.app.use(helmet());
+    this.app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, '..', 'uploads')));
+  }
 
-
-const corsOptions = {
-  origin(origin, callback) {
-      if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-        callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-  
-*/
+  routes() {
+    this.app.use('/', homeRoutes);
+    this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
+    this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
+  }
+}
 
 export default new App().app;
